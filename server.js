3b3c120e959182fe74faf4d5ab5f208644b9b522
jimezarate27 <jzarate30138@ufide.ac.cr>
2024-06-27 20:39:26 -0600

    Navigation ready

--- server.js
+++ server.js
@@ -3,14 +3,36 @@ const path = require('path');
 
 const app = express();
 const PORT = 3000;
+const PUBLIC= path.join(__dirname, 'public');
 
 app.use(express.urlencoded({ extended: true}));
+app.use(express.static(PUBLIC));
 
 app.get('/', (req, res) => {
-    console.log('Loading home...');
-    res.send('Hello world');
+    console.log('Loading home...\n');
+    res.sendFile(path.join(PUBLIC, 'home.html'));
+});
+
+app.get('/contactus', (req, res) => {
+    console.log('Loading contact us...');
+    res.sendFile(path.join(PUBLIC, 'contactus.html'));
+});
+
+app.post('/contactus',(req, res) => {
+    console.info('Contact Us has been called...\n');
+    const name = req.body.name;
+    const email = req.body.email;
+    const subject = req.body.subject;
+
+    console.log('Form data\n');
+    console.log('Name: ' + name);
+    console.log('Email: ' + email);
+    console.log('Subject: ' + subject);
+
+    res.redirect('/');
 });
 
 app.listen(PORT, () => {
-    console.log('💡 Server is running on port '+ PORT);
+    console.log('💡🪼 Server is running on port '+ PORT);
 });
+
